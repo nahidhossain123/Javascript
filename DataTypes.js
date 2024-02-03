a23e2e3715d3e4b8e2682c215d132b0913db53e6
nahidhossain123 <nahidhossain351@gmail.com>
2024-02-03 23:54:46 +0600

    Javascript data types

---
+++ DataTypes.js
@@ -0,0 +1,43 @@
+//Javascript data types
+//Dta Types----->
+//i. Primaitive Data Type
+//ii. Non Primitive Data type
+
+//Primaitive Data Type:
+//1. String
+//2. Number
+//3. Boolean
+//4. Undefined
+//5. Null
+
+//Non-Primaitive Data Type:
+//1. Object
+//2. Array
+//3. RegExp
+
+//Number:
+let length = 50;
+let weight = 60.6;
+
+//String:
+let firstName = "Nahid";
+let lastName = "Hossain";
+
+//Boolean:
+let isMale = true;
+let isMarried = false;
+
+//Object:
+const userInfo = {
+  firstName: "Nahid",
+  lastName: "Hossain",
+  age: 27,
+  isMale: true,
+  isMarried: false,
+};
+
+//Array Object
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2024-02-3");
